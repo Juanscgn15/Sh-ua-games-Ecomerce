231771d32fc8948d6881e248b0d5e1d692fb18ee
Juanscgn15 <102631955+Juanscgn15@users.noreply.github.com>
2023-02-07 22:57:48 -0300

    first commit

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ItemListContainer = ({greeting}) => {
   return (
-    <div className="container-fluid">
-    <div className="align-items-middel">
+    <div className="container-fluid text-bg-primary p-3">
+    <div>
 <p > {greeting} </p>
     </div>
     </div> )
